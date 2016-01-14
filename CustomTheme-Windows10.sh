3fc503be8dcab03c481cf090a112be996833ce96
Randson Nunes <randson_1@yahoo.com.br>
2016-01-14 14:53:33 -0300

    Create CustomTheme-Windows10
    Create CustomTheme-Windows10

---
+++ CustomTheme-Windows10.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "============================================"
+echo "   instalando tema do Windows 10"
+echo "============================================"
+sudo add-apt-repository ppa:noobslab/themes
+sudo apt-get update
+sudo apt-get install windos-10-themes
